31da2f40e86fa8adf679a142109225e983325348
hithyshi <hithyshi.krishnamurthy@ibm.com>
2022-02-25 14:18:01 +0000

    adding add functionality

--- main.go
+++ main.go
@@ -1,10 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
 func say_hello() string {
 
 	return "Hello Hello"
+}
+
+func add(input string) (*float64, error) {
+	var number, add float64
+	var err error
+	numbers := strings.Split(input, ",")
+	fmt.Println(numbers)
+	fmt.Printf("Length of string is %v", len(numbers))
+	for _, numberString := range numbers {
+		if numberString == "" {
+			continue
+		}
+		if number, err = strconv.ParseFloat(numberString, 32); err != nil {
+			fmt.Println("Cannot convert string to float", err)
+			return nil, err
+		}
+		fmt.Printf("Converted Number is %v\n", number)
+		//fmt.Printf("Add value is %v\n", add)
+		add = add + number
+		//fmt.Printf("Add value is %v\n", add)
+	}
+	fmt.Printf("Add value is %v\n", add)
+	return &add, nil
 }
 
 func main() {
--- main_test.go
+++ main_test.go
@@ -1,9 +1,13 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func Test_say_hello(t *testing.T) {
-
 
 	tests := []struct {
 		name string
@@ -19,6 +23,58 @@ func Test_say_hello(t *testing.T) {
 			if got := say_hello(); got != tt.want {
 				t.Errorf("say_hello() = %v, want %v", got, tt.want)
 			}
+		})
+	}
+}
+
+func Test_add(t *testing.T) {
+	tests := []struct {
+		name    string
+		input   string
+		want    float64
+		wantErr bool
+	}{
+		{
+			name:  "good path with valid numbers",
+			input: "13,41",
+			want:  54.0,
+		},
+		{
+			name:    "invalid numbers as input with string in it",
+			input:   "13a,41",
+			wantErr: true,
+		},
+		{
+			name:    "only one number as input and nothing after comma",
+			input:   "13,",
+			want:    13.0,
+			wantErr: false,
+		},
+		{
+			name:    "invalid second number as input with string in it",
+			input:   "13,41b",
+			wantErr: true,
+		},
+		{
+			name:    "invalid second number as input with string in it",
+			input:   "13 ,41",
+			wantErr: true,
+		}, {
+			name:    "passing float values as input",
+			input:   "13.9,41.2",
+			want:    55.1,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := add(tt.input)
+			if err == nil {
+				fmt.Printf("Value of *got is %v\n", *got)
+				assert.Equal(t, *got, tt.want, "these should be equal")
+			}
+			assert.Equal(t, err != nil, tt.wantErr)
+
 		})
 	}
 }
